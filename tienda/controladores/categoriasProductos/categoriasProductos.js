b7621167cbe72dcb2cee59bf628641a5c061860b
felipe-segovia <143759036+felipe-segovia@users.noreply.github.com>
2024-11-24 16:43:31 -0300

    nombre categorias

--- tienda/controladores/categoriasProductos/categoriasProductos.js
+++ tienda/controladores/categoriasProductos/categoriasProductos.js
@@ -7,16 +7,16 @@ const htmlCategorias = `
         </div>
         <div class="category-box category-2">
             <img src="/img/categorias/imagen2.png" alt="Categoría 2">
-            <p>Categoría 2</p>
+            <p>Gafas de Sol</p>
         </div>
         <!-- Segunda fila -->
         <div class="category-box category-3">
             <img src="/img/categorias/imagen3.png" alt="Categoría 3">
-            <p>Categoría 3</p>
+            <p>Brazalates</p>
         </div>
         <div class="category-box category-4">
             <img src="/img/categorias/imagen4.png" alt="Categoría 4">
-            <p>Categoría 4</p>
+            <p>Collares</p>
         </div>
     </div>
 `;
