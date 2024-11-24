cb7158afbd1829b13a9101dbe248a608127d6020
Benjamín Peralta <benjaminp158@gmail.com>
2024-11-24 18:05:23 -0300

    Añadido funcionalidad al carrusel

--- tienda/controladores/categoriasProductos/categoriasProductos.js
+++ tienda/controladores/categoriasProductos/categoriasProductos.js
@@ -1,24 +1,24 @@
 const htmlCategorias = `
-    <div class="categories">
-        <!-- Primera fila -->
-        <div class="category-box category-1">
-            <img src="/img/categorias/imagen1.png" alt="Categoría 1">
-            <p>Relojes</p>
-        </div>
-        <div class="category-box category-2">
-            <img src="/img/categorias/imagen2.png" alt="Categoría 2">
-            <p>Gafas de Sol</p>
-        </div>
-        <!-- Segunda fila -->
-        <div class="category-box category-3">
-            <img src="/img/categorias/imagen3.png" alt="Categoría 3">
-            <p>Brazaletes</p>
-        </div>
-        <div class="category-box category-4">
-            <img src="/img/categorias/imagen4.png" alt="Categoría 4">
-            <p>Collares</p>
-        </div>
-    </div>
+<div class="categories">
+    <!-- Primera fila -->
+    <a href="#RELOJES" class="category-box category-1">
+        <img src="/img/categorias/imagen1.png" alt="Categoría 1">
+        <p>Relojes</p>
+    </a>
+    <a href="#GAFAS" class="category-box category-2">
+        <img src="/img/categorias/imagen2.png" alt="Categoría 2">
+        <p>Gafas de Sol</p>
+    </a>
+    <!-- Segunda fila -->
+    <a href="#BRAZALETES" class="category-box category-3">
+        <img src="/img/categorias/imagen3.png" alt="Categoría 3">
+        <p>Brazaletes</p>
+    </a>
+    <a href="#COLLARES" class="category-box category-4">
+        <img src="/img/categorias/imagen4.png" alt="Categoría 4">
+        <p>Collares</p>
+    </a>
+</div>
 `;
 
 export function categoriasProductos() {
